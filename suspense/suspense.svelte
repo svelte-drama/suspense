7e844839af0152f6ca88f52847d841ad0aeb4dfd
Brian Keyes <b.keyes@blueglassmoon.com>
2021-10-17 06:55:12 -0500

    Simpler logic to track internal state of Suspense component

--- suspense/suspense.svelte
+++ suspense/suspense.svelte
@@ -19,114 +19,95 @@ export function createSuspense() {
 
 <script>
 import { createEventDispatcher, setContext } from 'svelte'
-import { writable } from 'svelte/store'
+import { readable } from 'svelte/store'
 import { CONTEXT as LIST_CONTEXT, STATUS as LIST_STATUS } from './suspense-list.svelte'
 const dispatch = createEventDispatcher()
-
-const STATUS = {
-  INIT: 0,
-  LOADING: 1,
-  ERROR: 2,
-  READY: 3
-}
-
-let error = null
-let pending = 0
-let state = STATUS.INIT // FIXME: This needs to set to LOADING for SSR
 
 const register = getContext(LIST_CONTEXT)
 const {
   onReady = () => {},
-  status: list_status = writable(LIST_STATUS.READY)
+  status: list_status = readable(LIST_STATUS.READY)
 } = register?.() ?? {}
 setContext(LIST_CONTEXT)
 
 setContext(CONTEXT, suspend)
 
-updateState()
+let error
+let loading = false
+
+// Debounce and give this a slight delay to account for
+// new promises coming in as a result of old ones resolving.
+const dispatchLoadEvent = debounce(() => {
+  if (!loading) {
+    onReady()
+    dispatch('load')
+  }
+}, 5)
+
+// Svelte stores track if they have any active subscribers.
+// We'll take advantage of that logic, even though we never
+// care about the actual value of this store.
+const ref_count = readable(0, () => {
+  loading = true
+  return () => {
+    loading = false
+    dispatchLoadEvent()
+  }
+})
 
 /* ----- */
 
-export function suspend (promise = null, { timeout } = {}) {
-  let once = true
-  pending += 1
-  updateState()
+function createPromise () {
+  let resolve, reject
+  const promise = new Promise((resolve_, reject_) => {
+    resolve = resolve_
+    reject = reject_
+  })
+  return { promise, resolve, reject }
+}
 
-  function resolve () {
-    if (once) {
-      once = false
-      pending -= 1
-      updateState()
-    }
-  }
+function debounce (fn, time) {
+	let timer
 
-  function reject (err) {
-    if (once) {
-      once = false
-      error = err
-      pending -= 1
-      state = STATUS.ERROR
-      dispatch('error', e)
-    }
-  }
+	return function (...args) {
+		clearTimeout(timer)
+		timer = setTimeout(() => fn(...args), time)
+	}
+}
 
-  if (!promise) {
+function suspend (promise_like) {
+  if (!promise_like) {
+    const { promise, resolve, reject } = createPromise()
+    suspend(promise)
     return { resolve, reject }
   }
 
-  const condition = (timeout ?
-    Promise.race([promise, wait(timeout)]) :
-    promise
-  )
-  return condition
-    .then(result => {
-      resolve()
-      return result
-    })
+  const unsubscribe = ref_count.subscribe(() => {})
+  return promise_like
     .catch(e => {
-      reject(e)
-      throw e
+      error = e
+      dispatch('error', e)
     })
-}
-
-function updateState () {
-  if (state == STATUS.ERROR) return
-
-  function update() {
-    if (state === STATUS.ERROR) return
-    if (pending) {
-      state = STATUS.LOADING
-    } else {
-      // TODO: Should we allow components to go from READY to LOADING?
-      // The UX is pretty awful on every example I've tried.
-      state = STATUS.READY
-      onReady()
-      dispatch('load')
-    }
-  }
-
-  if (typeof window.requestIdleCallback === 'function') {
-    requestIdleCallback(update, {
-      timeout: 50
-    })
-  } else {
-    setTimeout(update, 10)
-  }
-}
-
-function wait (timeout) {
-  return new Promise(resolve => setTimeout(resolve, timeout))
+    .finally(unsubscribe)
 }
 </script>
 
-{#if state === STATUS.ERROR}
+{#if error}
   <slot name="error" { error }></slot>
-{:else if $list_status === LIST_STATUS.HIDDEN}
-  <!-- Hidden -->
-{:else if state === STATUS.LOADING || $list_status === LIST_STATUS.LOADING}
-  <slot name="loading"></slot>
+{:else}
+  {#if $list_status === LIST_STATUS.HIDDEN}
+    <!-- Hidden -->
+  {:else if loading || $list_status === LIST_STATUS.LOADING}
+    <slot name="loading"></slot>
+  {/if}
+
+  <div hidden={ loading || $list_status !== LIST_STATUS.READY }>
+    <slot { suspend }></slot>
+  </div>
 {/if}
 
-<div hidden={ state !== STATUS.READY || $list_status !== LIST_STATUS.READY }>
-  <slot { suspend }></slot>
-</div>
+<style>
+div:not([hidden]) {
+  display: contents;
+}
+</style>
